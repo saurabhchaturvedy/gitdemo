7a028c66b4b96c8f9880ef80b0cff172ffc77d73
Chaturvedi,Saurabh(sc794x) <sc794x@att.com>
2019-10-15 21:42:22 +0530

    [demo-git] added init project to local repo .

---
+++ src/main/java/com/test/main/Main.java
@@ -0,0 +1,13 @@
+package com.test.main;
+
+public class Main {
+
+	public String addData() {
+
+		return "App is initializing";
+	}
+
+	public static void main(String[] args) {
+		System.out.println("The app is running ...");
+	}
+}
