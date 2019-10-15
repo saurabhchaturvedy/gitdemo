21f6b8d017271900b72d176f6e579e10376b03c2
saurabhchaturvedy <56601821+saurabhchaturvedy@users.noreply.github.com>
2019-10-15 22:14:40 +0530

    Update Main.java

--- src/main/java/com/test/main/Main.java
+++ src/main/java/com/test/main/Main.java
@@ -6,6 +6,11 @@ public class Main {
 
 		return Constants.APP_NAME + " is initializing";
 	}
+	
+	public String demoGitPull() {
+
+		return "added to demonstrate the Git Pull";
+	}
 
 	public static void main(String[] args) {
 		System.out.println("The app is running ...");
