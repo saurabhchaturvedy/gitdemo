55be5b35789323856b05cbdd8a7c2cfe659add2d
Saurabh Chaturvedi <56601821+saurabhchaturvedy@users.noreply.github.com>
2020-07-20 14:07:20 +0530

    Update Constants.java

--- src/main/java/com/test/main/Constants.java
+++ src/main/java/com/test/main/Constants.java
@@ -3,4 +3,6 @@ package com.test.main;
 public class Constants {
 
 	public static final String APP_NAME = "myApp";
+	public static final String APP_NAME2 = "myApp";
+	public static final String APP_NAME3 = "myApp";
 }
